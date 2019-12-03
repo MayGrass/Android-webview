c2bc258827add7a3b1b190732e4e9168d7073202
DCH <dching0121@gmail.com>
2019-12-03 16:25:51 +0800

    javascript google map

--- app/src/main/java/tw/org/iii/webview/MainActivity.java
+++ app/src/main/java/tw/org/iii/webview/MainActivity.java
@@ -1,7 +1,17 @@
 package tw.org.iii.webview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -16,15 +26,80 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
     private EditText editText;
+    private LocationManager locationManager;
+    private MyListener myListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    7777);
+        }else{
+            init();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            init();
+        }else{
+            finish();
+        }
+    }
+
+    private void init() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        myListener = new MyListener();
 
         editText = findViewById(R.id.edittext);
         webView = findViewById(R.id.webView);
         initWebView();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER
+                , 0, 0,myListener);
+    }
+
+    private class MyListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            double lat = location.getLatitude();
+            double lng = location.getLongitude();
+            Log.v("DCH", lat + "x" + lng);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(myListener);
     }
 
     private void initWebView() {
@@ -38,7 +113,8 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setBuiltInZoomControls(true);
         //webSettings.setDisplayZoomControls(true);
         webView.addJavascriptInterface(new MyJS(), "DCH");
-        webView.loadUrl("file:///android_asset/bootstrap.html");
+//        webView.loadUrl("file:///android_asset/bootstrap.html");
+        webView.loadUrl("file:///android_asset/map.html");
 //        webView.loadUrl("https://www.iii.org.tw");
     }
 
