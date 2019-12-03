05613468cce30681e53b2ce0dc481574727f9818
DCH <dching0121@gmail.com>
2019-12-03 14:11:07 +0800

    Web相關設定以及自訂html

--- app/src/main/java/tw/org/iii/webview/MainActivity.java
+++ app/src/main/java/tw/org/iii/webview/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -23,7 +24,15 @@ public class MainActivity extends AppCompatActivity {
     private void initWebView() {
         WebViewClient webViewClient = new WebViewClient();
         webView.setWebViewClient(webViewClient);
-        webView.loadUrl("https://www.iii.org.tw");
+        //網頁相關設定
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); //開啟javascript
+        //開啟縮放功能
+//        webSettings.setSupportZoom(true);
+//        webSettings.setBuiltInZoomControls(true);
+//        webSettings.setDisplayZoomControls(true);
+        webView.loadUrl("file:///android_asset/jquery_animate.html");
+//        webView.loadUrl("https://www.iii.org.tw");
     }
 
     //預設案返回會結束程式，寫條件讓網站可以按返回變成上一頁
